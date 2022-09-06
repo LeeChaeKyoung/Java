5be5daa4890b0818bb598a3007376d7f9162df17
jaek81 <jaek81@gmail.com>
2022-09-06 17:02:20 +0900

    Day1-2

--- 01/operator.java
+++ 01/operator.java
@@ -90,7 +90,7 @@ import javax.swing.plaf.synth.SynthSeparatorUI;
     }
 }*/
 
-class OperaterEx10{
+/*class OperaterEx10{
     public static void main(String[] args){
         int a = 1000000;
 
@@ -100,6 +100,39 @@ class OperaterEx10{
         System.out.println(result1); //먼저 오버플로우가 나서 다른 값이됨
         System.out.println(result2);
     }
-}
+}*/
 
+/*class OperaterEx11{
+    public static void main(String[] args){
+       char a = 'a';
+       char d = 'd';
+       char zero = '0';
+       char two = '2';
+
+       System.out.printf("'%c' -'%c'= %d%n", d, a, d-a);
+       System.out.printf("'%c' -'%c'= %d%n", two, zero, two-zero);
+       System.out.printf("'%c' = %d%n", a, (int)a);
+       System.out.printf("'%c' = %d%n", d, (int)d);
+       System.out.printf("'%c' = %d%n", two, (int)two);
+       System.out.printf("'%c' = %d%n", zero, (int)zero);
+    }
+}*/
+
+class OperaborEx12{
+    public static void main(String[] orgs){
+        char c1 = 'a';
+        char c2 = c1;
+        char c3 = ' ';
+
+        int i = c1+1;
+
+        c3 = (char)(c1 +1);
+        c2++;
+        c2++;
+
+        System.out.println("i = "+i);
+        System.out.println("c2 = "+c2);
+        System.out.println("c3 = "+c3);
+    }
+}
 
